39081732cf627fbce0a3e4559501b395c5bd80aa
dancingpixels <bluegrasspro@yahoo.com>
2025-08-03 07:19:11 +0100

    modify build script

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,15 @@
-User.create!(
-  name:  "Super Admin",
-  email: "admin1@example.com",
-  password: "password",
-  password_confirmation: "password",
-  role: :admin
-)
+# User.create!(
+#   name:  "Super Admin",
+#   email: "admin1@example.com",
+#   password: "password",
+#   password_confirmation: "password",
+#   role: :admin
+# )
 
-User.create!(
-  name: "Top Salesperson",
-  email: "sales1@example.com",
-  password: "password",
-  password_confirmation: "password",
-  role: :sales
-)
+# User.create!(
+#   name: "Top Salesperson",
+#   email: "sales1@example.com",
+#   password: "password",
+#   password_confirmation: "password",
+#   role: :sales
+# )
